8347e6fe8f7587aaabd3456d4849e1b86bde9b18
terracubist <tfung@hmc.edu>
2026-03-20 16:25:57 -0700

    Update source.js

--- source.js
+++ source.js
@@ -11,3 +11,5 @@ function openCity(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+document.getElementById("defaultOpen").click();
