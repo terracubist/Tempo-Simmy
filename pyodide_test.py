e82b3bc4341b313ad90268c40c860ef1070524b8
terracubist <tfung@hmc.edu>
2026-03-27 15:42:20 -0700

    add pdb

--- pyodide_test.py
+++ pyodide_test.py
@@ -1,9 +1,12 @@
-import sys
+import pdb
+pdb.set_trace()
+
+# import sys
 
 
-# import pandas as pd
-# import numpy as np
+# # import pandas as pd
+# # import numpy as np
 
-print(f"sys.version is: {sys.version}")
-# print(f"pd.version is: {pd.__version__}")
-# print(f"np.version is: {np.__version__}")
+# print(f"sys.version is: {sys.version}")
+# # print(f"pd.version is: {pd.__version__}")
+# # print(f"np.version is: {np.__version__}")
