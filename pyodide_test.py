6d6f5ac02c4096db8e5f160ce872c8d45e5f3f54
terracubist <tfung@hmc.edu>
2026-03-27 15:25:45 -0700

    rm pd and np checks

--- pyodide_test.py
+++ pyodide_test.py
@@ -3,5 +3,5 @@ import pandas as pd
 import numpy as np
 
 print(f"sys.version is: {sys.version}")
-print(f"pd.version is: {pd.__version__}")
-print(f"np.version is: {np.__version__}")
+# print(f"pd.version is: {pd.__version__}")
+# print(f"np.version is: {np.__version__}")
